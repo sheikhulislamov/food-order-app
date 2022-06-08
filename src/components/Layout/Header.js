5c88959127022a87698df418456be7c72a80690f
Arsen <arsen.sheikhulislamov@protonmail.com>
2022-06-08 03:44:59 +0300

    Add Header Component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+
+const Header = props => {
+    return <Fragment>
+        <header>
+            <h1>ReactMeals</h1>
+            <button>Cart</button>
+        </header>
+        <div>
+            <img src="" alt="" />
+        </div>
+    </Fragment>
+};
+
+export default Header;
